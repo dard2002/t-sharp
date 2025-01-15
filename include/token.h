34edc77d5c6d11012b4ea56805adf37505c27ce0
dard2002 <dylan.5software@gmail.com>
2025-01-15 22:19:43 +1100

    Started working on token types

--- include/token.h
+++ include/token.h
@@ -3,10 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Types of tokens defined in T#
 enum { 
-    TK_KEYWORD,
-    TK_VARIABLE_NAME,
-    TK_EOF
+  TK_TYPE,
+  TK_FUNCTION_NAME,
+  TK_BLOCK_START,
+  TK_FUNCTION_ARGS,
+  TK_BLOCK_END,
+  TK_VARIABLE_NAME,
+  TK_OPERATOR,
+  TK_LINE_END
 };
 
 typedef struct {
